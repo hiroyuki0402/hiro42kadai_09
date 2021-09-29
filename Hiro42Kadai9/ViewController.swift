5aea8df56d2c9d2bd990c7465549f46e888748f3
HiroyukiShiraishi <hiro19900402@gmail.com>
2021-09-30 01:22:45 +0900

    Initial Commit

---
+++ Hiro42Kadai9/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Hiro42Kadai9
+//
+//  Created by 白石裕幸 on 2021/09/30.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
