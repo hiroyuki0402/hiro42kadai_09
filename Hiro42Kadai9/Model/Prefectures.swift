b027163a5a1683644fe74532bec93f134e654b74
akio0911 <akio0911@gmail.com>
2021-10-04 20:06:22 +0900

    feedback

--- Hiro42Kadai9/Model/Prefectures.swift
+++ Hiro42Kadai9/Model/Prefectures.swift
@@ -7,10 +7,6 @@
 
 import Foundation
 
-protocol PrefecturesDataProtocol: AnyObject {
-    func prefectureData(data: String)
-}
-
 class Prefectures {
     static func prefectures() -> [String] {
         return  [ "東京都", "神奈川県", "埼玉県", "千葉県" ]
